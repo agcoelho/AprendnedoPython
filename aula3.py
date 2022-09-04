326bfe7259e58dc74a619cb5cbd49827758fade6
agcoelho <78510085+agcoelho@users.noreply.github.com>
2022-09-03 23:47:42 -0400

    aula operadores logicos

--- aula3.py
+++ aula3.py
@@ -1,5 +1,10 @@
 n1 = int(input('digite um numero\n'))
 s1 = n1 - 1
 s2 = n1 + 1
+n2 = int(input('digite outro numero'))
+a1 = n2 * 2
+a2 = n2 * 3
+a3 = n2**(1/2)
 
-print('o antecessor é: {} e o sucessor é: {}'.format(s1, s2))
+print('o antecessor do primeiro numero  é: {} e o sucessor é: {}'.format(s1, s2))
+print('o dobro do segundo numero é: {}, o triplo do segundo numero é: {}, a raiz quadrada do segundo numero é: {}'.format(a1, a2, a3))
