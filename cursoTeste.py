6b07de95188c2334eba7073a5fae8fb548af8eb3
agcoelho <78510085+agcoelho@users.noreply.github.com>
2022-09-03 23:10:08 -0400

    exercicio aniversario

--- cursoTeste.py
+++ cursoTeste.py
@@ -1,1 +1,5 @@
-print('teste')
+nome = input('digite seu nome \n')
+dia = input('digite o dia de seu nascimento\n')
+mes = input('digite o mês de seu nascimento\n')
+ano = input('digite o ano de seu nascimento\n')
+print('vc nasceu em: '+dia+'/'+mes+'/'+ano)
