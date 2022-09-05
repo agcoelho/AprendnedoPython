a6b96f7003c97b8c09c592027933e2a82c51ddca
agcoelho <78510085+agcoelho@users.noreply.github.com>
2022-09-05 01:16:44 -0400

    for

---
+++ teste.py
@@ -0,0 +1,11 @@
+import time
+
+for c in range(10,0,-1):
+    print('{}'.format(c))
+    time.sleep(1)
+print('os fogos foram soltos ebaaaa!!!!')
+
+for c in range(2,51,2):
+    print(c)
+print('os numeros pares estão acima')
+
