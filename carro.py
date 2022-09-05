0957507cce528c0b2b02881d836aa92b4bf674b7
agcoelho <78510085+agcoelho@users.noreply.github.com>
2022-09-04 20:33:31 -0400

    escreva um programa q leia a velocidade de um carro. se ele ultrapassar 80km/h, mostre uma mensagem dizendo q ele foi multado. a multa deve custar 7 reais por cada km acima do limite

---
+++ carro.py
@@ -0,0 +1,10 @@
+import random
+
+velo = random.randint(50, 100)
+
+if velo > 80:
+    multa = (velo - 80) * 7
+    print('vc ultrapassou a velocidade com {}Km/h sua multa é de {} reais'.format(velo, multa))
+
+else:
+    print('parabens vc está a {}Km/h'.format(velo))
